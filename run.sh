063446264d7034ccbc00bab08ee97fea0a829049
Alexandru Vezeteu <alexanteu@gmail.com>
2024-11-29 10:39:32 +0200

    test run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 # loc in care vom scrie comenzi de update
 
 #toate comenzile scrise aici se executa pe toate masinile virtuale la start up
 
 #are rol de debug/faciliteaza munca
+
+
+echo TEST
