de6df2a508a7372388770960a1d822c93761655a
Asaftei Rares <rares-stefan.asaftei@student.tuiasi.ro>
2024-12-01 15:24:17 +0200

    __str__ + unpack pt mesaj

--- Message.py
+++ Message.py
@@ -30,8 +30,8 @@ class Message:
                    ripEntry.next_hop, 
                    ripEntry.metric)
         
-    def unpack_bytes(self, bytes):
-        unpacked_data = struct.unpack('!2BH2H4I', bytes)
+    def unpack_bytes(self, mesaj_impachetat):
+        unpacked_data = struct.unpack('!2BH2H4I', mesaj_impachetat)
         self.command = unpacked_data[0]
         self.version = unpacked_data[1]
         self.must_be_zero = unpacked_data[2]
@@ -41,6 +41,7 @@ class Message:
         self.subnet = unpacked_data[6]
         self.next_hop = unpacked_data[7]
         self.metric = unpacked_data[8]
+        
         
         
     def pack_bytes(self):
@@ -61,11 +62,19 @@ class Message:
                     self.next_hop, # 4
                     self.metric) # 4  
         
+    def __str__(self):
+        for eticheta , val in vars(self).items():
+            print(f'{eticheta} : {val}')
+            
     
-# message = Message.with_ripEntry(1, 2, 0, RIPEntry(0x02, 0x00, 0x00000000, 0x00000000, 0x00000000, 0x00000000))
-# message2 = Message(1, 2, 0, 0x02, 0x00, 0x00000000, 0x00000000, 0x00000000, 0x00000000)
+# TEST !!!!
+#message = Message.with_ripEntry(1, 2, 0, RIPEntry(0x02, 0x00, 0x00000000, 0x00000000, 0x00000000, 0x00000000))
+#mesaj_packed = message.pack_bytes() # mesajul impachetat
+#message.__str__() # afisez sa vad etichetele si valorile
+#print(mesaj_packed) # vad cum arata mesajul impachetat
 
-# print(message.pack_bytes()) # b'\x01\x02\x00\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
-# print(message2.pack_bytes()) # b'\x01\x02\x00\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+#message2 = Message(0, 0, 0, 0, 0, 0, 0, 0, 0)
+#message2.unpack_bytes(mesaj_packed)
+#message2.__str__()
 
-# pare ok -_- 
+
