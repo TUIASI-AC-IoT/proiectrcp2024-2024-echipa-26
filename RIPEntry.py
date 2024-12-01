29889074eb4475b14bd4abcdd6d3f9155ba3fa30
Asaftei Rares <rares-stefan.asaftei@student.tuiasi.ro>
2024-12-01 14:15:27 +0200

    RIPEntry.py constructor

--- RIPEntry.py
+++ RIPEntry.py
@@ -5,5 +5,13 @@ class RIPEntry:
     RIPEntry
     '''
     #de completat cu parametri si chestii
-    def __init__(self):
+    def __init__(self, address_family_identifier, route_tag, ipv4, subnet, next_hop, metric):
+        self.address_family_identifier = address_family_identifier
+        self.route_tag = route_tag
+        self.ipv4 = ipv4
+        self.subnet = subnet
+        self.next_hop = next_hop
+        self.metric = metric
+
+    def __str__(self):
         pass
