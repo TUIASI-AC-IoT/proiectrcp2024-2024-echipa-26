42b0df0a8c73b054ad9e24e34e3022283b516afe
Alexandru Vezeteu <alexanteu@gmail.com>
2024-12-02 11:41:52 +0200

    added more sockets to the listener

--- ProcessTarget.py
+++ ProcessTarget.py
@@ -3,7 +3,7 @@ from random import seed, randint
 import socket
 import struct
 import signal
-
+import select
 from Message import *
 
 from RoutingTable import *
@@ -19,34 +19,30 @@ def multicastListen(pipe, ipList):
     sleep(randint(1,10))
     multicastPort = 520
     multicastIP = '224.0.0.9'
-    
-    #de mutat trimis-ul de request catre sender?
+
+    socketList =[]
+
     for ip in ipList:
         sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, proto=17)
+        sender.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         sender.bind((ip, multicastPort))
-        sender.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 1)
-        sender.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_LOOP, 0)
-        sender.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_IF, socket.inet_aton(ip))
-        #build message
-        sender.sendall()
-        sender.close()
-        #send req
-
-    pipe.send(0)
-
-    
+        socketList.append(sender)
     receiver = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, proto =17)
     receiver.bind((multicastIP, multicastPort))
     for ip in ipList:
         r = struct.pack("=4s4s", socket.inet_aton(multicastIP), socket.inet_aton(ip))
         receiver.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, r)
+    socketList.append(receiver)
 
     while True:
-        data, s = receiver.recvfrom(1024)
-        msg = toMessage(data)
-        toSend = (msg, s[0])
-        #s - tuplu s[0]-adresa ip si s[1]-portul
-        pipe.send(toSend)
+        ready_to_read, _,_ = select.select(socketList,[],[], 0.1)
+
+        for receiver in ready_to_read:
+            data, s = receiver.recvfrom(1024)
+            msg = toMessage(data)
+            toSend = (msg, s[0])
+            #s - tuplu s[0]-adresa ip si s[1]-portul
+            pipe.send(toSend)
                 
 
 
@@ -54,11 +50,9 @@ def multicastSender(pipe, ipList):
     socketDict = dict()
     multicast = (multicastIP, multicastPort)
 
-    # astept ca listener-ul sa trimita request-urile (nu pot face bind la adresele ip pe mai multe sockets)
-    pipe.recv()
-
     for ip in ipList:
         sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, proto=17)
+        sender.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         sender.bind((ip[0], multicastPort))
         sender.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 1)
         sender.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_LOOP, 0)
