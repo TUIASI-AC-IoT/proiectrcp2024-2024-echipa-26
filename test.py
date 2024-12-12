a1f8d385d87e0a31b0050dc05936a4879654af7b
Alexandru Vezeteu <alexanteu@gmail.com>
2024-12-13 01:36:15 +0200

    test

---
+++ test.py
@@ -0,0 +1,46 @@
+import socket
+import struct
+import select
+
+multicastPort = 520
+multicastIP = '224.0.0.9'
+
+listenIP=['192.168.1.1', '192.168.2.1']
+sendIP = ['192.168.1.2', '192.168.3.2']
+
+def listen():
+    socket_list =[]
+    for ip in listenIP:
+        sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, proto=17)
+        sender.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sender.bind((ip, multicastPort))
+        sender.bind((multicastIP, multicastPort))
+        r = struct.pack("=4s4s", socket.inet_aton(multicastIP), socket.inet_aton(ip))
+        sender.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, r)
+
+        socket_list.append(sender)
+        
+    while True:
+        ready_to_read, _,_ = select.select(socket_list,[],[], 0.1)
+        for receiver in ready_to_read:
+            data, s = receiver.recvfrom(1024)
+            print(data.decode('ascii')+' '+str(s) + ' '+str(receiver.gethostname()))
+            
+            
+            
+
+def send():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, proto=17)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind(('', multicastPort))
+    s.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 1)
+    s.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_LOOP, 0)
+    for ip in sendIP:
+        s.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_IF, socket.inet_aton(ip))
+        
+    s.sendto(bytes('hey', 'ascii'), (listenIP[0], multicastPort))
+    s.sendto(bytes('multicast', 'ascii'), (multicastIP, multicastPort))
+        
+
+if __name__=="__main__":
+    pass
