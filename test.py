671b370141a75c059c4faab717ba345ad97322b3
Alexandru Vezeteu <alexanteu@gmail.com>
2024-12-13 21:53:24 +0200

    another day, another test

--- test.py
+++ test.py
@@ -10,22 +10,27 @@ sendIP = ['192.168.1.2', '192.168.3.2']
 
 def listen():
     
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, proto=17)
-    s.sender.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind(('', multicastPort))
-    
+    socketList =[]
+
+    #sockets pt raspunsuri trimise direct
+    for ip in listenIP:
+        sender = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, proto=17)
+        sender.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sender.bind((ip[0], multicastPort))
+        socketList.append(sender)
     
     receiver = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, proto=17)
     receiver.bind((multicastIP, multicastPort))
+
     for ip in listenIP:
-        r = struct.pack("=4s4s", socket.inet_aton(multicastIP), socket.inet_aton(ip))
+        r = struct.pack("=4s4s", socket.inet_aton(multicastIP), socket.inet_aton(ip[0]))
         receiver.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, r)
 
-    socket_list =[s, receiver]
+    socketList.append(receiver)
         
         
     while True:
-        ready_to_read, _,_ = select.select(socket_list,[],[], 0.1)
+        ready_to_read, _,_ = select.select(socketList,[],[], 0.1)
         for receiver in ready_to_read:
             data, s = receiver.recvfrom(1024)
             print(data.decode('ascii')+' '+str(s) + ' '+str(receiver.getsockname()))
