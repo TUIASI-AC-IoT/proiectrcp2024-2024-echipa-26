c086da33de230d8f78456b53e87be0ed4e13b56d
Alexandru Vezeteu <alexanteu@gmail.com>
2025-01-15 14:36:23 +0200

    added a lock for the getallentries problem method

--- SharedTable.py
+++ SharedTable.py
@@ -27,6 +27,7 @@ class SharedTable:
         self.objectManager = MyManager()
         self.objectManager.start()
         
+        self.managerLock = self.manager.Lock()
         
         self.entries = self.manager.dict()
         self.flags = self.manager.dict()
@@ -221,15 +222,15 @@ class SharedTable:
         '''
         Returns a list of all the entries in the table.
         '''
-        
-        ret = []
-        for IP in list(self.entries.keys()):
-            try:
-                e = RIPEntry(other=self.entries[IP])
-                ret.append(e)
-            except KeyError:
-                continue
-        return ret
+        with self.managerLock:
+            ret = []
+            for IP in list(self.entries.keys()):
+                try:
+                    e = RIPEntry(other=self.entries[IP])
+                    ret.append(e)
+                except KeyError:
+                    continue
+            return ret
     
     def getAllTimeout(self)->Dict[str,Timer]:
         ret = dict()
@@ -266,14 +267,15 @@ class SharedTable:
         '''
         Returns all the entries that are changed. Beware returned entries are marked as unchanged when added to the result.
         '''
-        ret = []
-        for IP in list(self.entries.keys()):
-            try:
-                if self.flags[IP] == Flags.CHANGED:
-                    e = RIPEntry(other=self.entries[IP])
-                    ret.append(e)
-                    self.flags[IP] = Flags.UNCHANGED
-            except KeyError:
-                continue
-        
-        return ret
+        with self.managerLock:
+            ret = []
+            for IP in list(self.entries.keys()):
+                try:
+                    if self.flags[IP] == Flags.CHANGED:
+                        e = RIPEntry(other=self.entries[IP])
+                        ret.append(e)
+                        self.flags[IP] = Flags.UNCHANGED
+                except KeyError:
+                    continue
+            
+            return ret
